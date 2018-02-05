13238e020dc9ab9ac0d963329e8b68d76ae98931
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-02-05 12:25:59 -0600

    Problem set to practice JavaScript "this"
    Solution to the function "sumEvenArguments" that implements a use for the "this" method "call()".

--- this/this-methods-practice-solution.js
+++ this/this-methods-practice-solution.js
@@ -22,7 +22,13 @@ Examples:
 */
 
 function sumEvenArguments(){
-    
+    var newArgs = [].slice.call(arguments);
+	return newArgs.reduce( function(acc, next){
+		if(next % 2 === 0){
+			return acc + next;
+        }
+		return acc;
+    }, 0);
 }
 
 /* 
