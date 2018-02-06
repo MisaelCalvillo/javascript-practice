8481f044bc626ffc6cb116ef8bba7374a3818126
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-02-05 20:15:09 -0600

    Problem set to practice JavaScript "this"
    Solution to the function "bind" that implements a use for the "this" method "apply()".

--- this/this-methods-practice-solution.js
+++ this/this-methods-practice-solution.js
@@ -128,7 +128,12 @@ Examples:
 */
 
 function bind(fn, thisArg){
-    
+    var outerArgs = [].slice.call(arguments, 2);
+	return function() {
+		var innerArgs = [].slice.call(arguments);
+		var allArgs = outerArgs.concat(innerArgs);
+		return fn.apply(thisArg, allArgs);
+    }
 }
 
 /* 
